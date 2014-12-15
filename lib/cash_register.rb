82d89dfe2c939e55b77b7fe04e43f9ddee647280
Zubair Desai <zubair.desai@gmail.com>
2014-12-14 20:46:03 -0800

    Change file structure to run CLI in seperate file

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -16,33 +16,4 @@ class CashRegister
     @total.round(2)
   end
 
-
-end
-
-
-input = ""
-
-cash_register = CashRegister.new
-
-while input != "quit"
-  puts "type 'quit' to exit, or use the cash_register: 'purchase (num)', 'pay (num)', 'total'"
-  input = gets.chomp
-  break if input == "quit"
-
-  if input.include? "purchase"
-    match_data = input.match(/[0-9.]+/)
-    value = match_data.to_a.slice(0).to_f
-    cash_register.purchase(value)
-  end
-
-  if input.include? "pay"
-    match_data = input.match(/[0-9.]+/)
-    value = match_data.to_a.slice(0).to_f
-    cash_register.pay(value)
-  end
-
-  if input.include? "total"
-    puts cash_register.total
-  end
-
 end
