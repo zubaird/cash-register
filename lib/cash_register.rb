f0e27fd6deef4dbec63b65a35e92693205614785
Zubair Desai <zubair.desai@gmail.com>
2014-12-14 20:42:49 -0800

    Complete exercise and add CLI for cash register

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,5 +1,48 @@
 class CashRegister
-  def total
-    0
+
+  attr_accessor :total
+
+  def initialize
+    @total = 0
   end
+
+  def purchase(number)
+    @total += number
+  end
+
+
+  def pay(number)
+    @total -= number.to_f
+    @total.round(2)
+  end
+
+
+end
+
+
+input = ""
+
+cash_register = CashRegister.new
+
+while input != "quit"
+  puts "type 'quit' to exit, or use the cash_register: 'purchase (num)', 'pay (num)', 'total'"
+  input = gets.chomp
+  break if input == "quit"
+
+  if input.include? "purchase"
+    match_data = input.match(/[0-9.]+/)
+    value = match_data.to_a.slice(0).to_f
+    cash_register.purchase(value)
+  end
+
+  if input.include? "pay"
+    match_data = input.match(/[0-9.]+/)
+    value = match_data.to_a.slice(0).to_f
+    cash_register.pay(value)
+  end
+
+  if input.include? "total"
+    puts cash_register.total
+  end
+
 end
--- spec/cash_register_spec.rb
+++ spec/cash_register_spec.rb
@@ -1,6 +1,7 @@
 require 'cash_register'
 
 describe CashRegister do
+
   describe "#total" do
     it 'starts at 0' do
       cash_register = CashRegister.new
@@ -9,5 +10,56 @@ describe CashRegister do
 
       expect(total).to eq(0)
     end
+
+    it "returns how much is owed when nothing is paid" do
+      cash_register = CashRegister.new
+
+      ammount_one = 30
+      ammount_two = 20
+
+      cash_register.purchase(ammount_one)
+      cash_register.purchase(ammount_two)
+      owed_ammount = cash_register.total
+
+      expect(owed_ammount).to eq(50)
+    end
+
   end
+
+  describe "#purchase" do
+    it "takes a floating number and adds it to the total" do
+      cash_register = CashRegister.new
+
+      cash_register.purchase(10.2)
+      total = cash_register.total
+
+      expect(total).to eq(10.2)
+    end
+  end
+
+  describe "#pay" do
+    it "takes a floating number for how much is paid, and returns how much is given." do
+      cash_register = CashRegister.new
+
+      cash_register.purchase(20.12)
+      total = cash_register.pay(20.10)
+
+      expect(total).to eq(0.02)
+
+    end
+
+
+  end
+
 end
+
+
+
+#
+# The Cash Register class should allow you to pass an amount to the cash register, calculate your total,
+# collect a payment and then return the balance after running through the program.
+#
+# There is a purchase method that takes a floating number and adds that to the total.
+# There is a total method that returns how much is owed.
+# There is a pay method that takes one floating number for how much is paid. It should return how much change is given.
+# When the Cash Register is initialized is should begin with a starting balance of 0
